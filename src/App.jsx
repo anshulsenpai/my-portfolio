d18cd0b3ff0d29641ba63bbc07d5696f73ce80b5
anshul kulkarni <anshulkulkarni00@gmail.com>
2024-04-26 00:10:00 +0530

    tweaked

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css';
 import Announcement from './components/Announcement/Announcement';
+import { ActionBtn } from './components/Hero/Hero.styled';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
+import oracle from "./Assets/oracle-manual.pdf"
+
 
 const App = () => {
   return (
     <div className="app">
-      <Announcement />
+      {/* <Announcement />
       <Navbar />
-      <Home />
+      <Home /> */}
+       <ActionBtn href={oracle} varient="download">Download</ActionBtn>
     </div>
   );
 }
